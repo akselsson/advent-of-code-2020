e415182f83da567dd5fdc1a1037d103d81ebb617
Patrik Akselsson <patrik.akselsson@tradera.com>
2020-12-01 16:37:33 +0100

    Day one problem 2

--- day_1/Program.cs
+++ day_1/Program.cs
@@ -13,6 +13,26 @@ namespace day_1
             var lines = await File.ReadAllLinesAsync("input.txt");
             var numbers = lines.Select(int.Parse).ToArray();
             FindPair(numbers);
+            FindTriplet(numbers);
+        }
+
+        private static void FindTriplet(int[] numbers)
+        {
+            for (int i = 0; i < numbers.Length - 2; i++)
+            {
+                for (int j = 1; j < numbers.Length -1 ; j++)
+                {
+                    for (int k = 2; k < numbers.Length; k++)
+                    {
+                        if (numbers[i] + numbers[j] + numbers[k] == 2020)
+                        {
+                            Console.WriteLine(numbers[i] * numbers[j] * numbers[k]);
+                            return;
+                        }
+                    }
+                    
+                }
+            }
         }
 
         private static void FindPair(int[] numbers)
