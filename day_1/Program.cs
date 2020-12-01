149535ef751fcd32ff1c0963c10c0b343c4ac207
Patrik Akselsson <patrik.akselsson@tradera.com>
2020-12-01 16:33:59 +0100

    Problem 1

---
+++ day_1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace day_1
+{
+    class Program
+    {
+        async static Task Main(string[] args)
+        {
+            var lines = await File.ReadAllLinesAsync("input.txt");
+            var numbers = lines.Select(int.Parse).ToArray();
+            FindPair(numbers);
+        }
+
+        private static void FindPair(int[] numbers)
+        {
+            for (int i = 0; i < numbers.Length - 1; i++)
+            {
+                for (int j = 1; j < numbers.Length; j++)
+                {
+                    if (numbers[i] + numbers[j] == 2020)
+                    {
+                        Console.WriteLine(numbers[i] * numbers[j]);
+                        return;
+                    }
+                }
+            }
+        }
+    }
+}
