ed0cec62a83d26c7fe9a108c562c4fee61134bec
Patrik Akselsson <patrik.akselsson@tradera.com>
2020-12-07 19:29:34 +0100

    Cleanup

--- Day7/Program.cs
+++ Day7/Program.cs
@@ -1,20 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations.Schema;
-using System.Diagnostics;
-using System.Diagnostics.Contracts;
 using System.IO;
 using System.Linq;
 using System.Text.RegularExpressions;
-using System.Threading;
-using System.Threading.Tasks.Sources;
 
 var file = args[0];
 var input = File.ReadAllLines(file);
 
-var containerPattern = new Regex(@"^(?<container_colour>[a-z ]*?) bags?");
-var contentPattern = new Regex(@"(?<count>\d+) (?<colour>[a-z ]*?) bags?");
-var rules = input.Select(Extract).ToArray();
+
+var rules = input.Select(ParseRules).ToArray();
 
 Console.WriteLine($"Part 1: {rules.Count(x => CanContain("shiny gold", x))}");
 Console.WriteLine($"Part 2: {CountBags("shiny gold",0)}");
@@ -24,15 +18,10 @@ Console.WriteLine($"Part 2: {CountBags("shiny gold",0)}");
 
 bool CanContain(string colour, BagRule rule)
 {
-
-    if (rule.Contents.Any(x => x.Colour == colour))
-    {
-        return true;
-    }
-
-    return rule.Contents.Any(x =>
-        rules.Where(y => y.Colour == x.Colour)
-            .Any(y => CanContain(colour, y)));
+    return rule.Contents.Any(x => 
+        x.Colour == colour ||
+        rules.Where(y => y.Colour == x.Colour).Any(y => CanContain(colour, y))
+        );
 
 }
 
@@ -48,18 +37,18 @@ int CountBags(string colour, int depth)
     return self + rule.Contents.Sum(x => x.Count * CountBags(x.Colour, depth + 1));
 }
 
-
-
-BagRule Extract(string line)
+BagRule ParseRules(string line)
 {
-    var match = containerPattern.Match(line);
-    if (!match.Success)
+    var containerPattern = new Regex(@"^(?<container_colour>[a-z ]*?) bags?");
+    var contentPattern = new Regex(@"(?<count>\d+) (?<colour>[a-z ]*?) bags?");
+    
+    var container = containerPattern.Match(line);
+    if (!container.Success)
     {
         throw new Exception("Failed to parse " + line);
     }
 
-    var colour = match.Groups["container_colour"].Value;
-
+    var colour = container.Groups["container_colour"].Value;
 
     return new BagRule
     {
