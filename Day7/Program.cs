c34875e1c2113f05797c3cff457a930af0a2f44d
Patrik Akselsson <patrik.akselsson@tradera.com>
2020-12-07 08:28:10 +0100

    day 7 part 1

---
+++ Day7/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Diagnostics.Contracts;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+var file = args[0];
+var input = File.ReadAllLines(file);
+
+var containerPattern = new Regex(@"^(?<container_colour>[a-z ]*?) bags?");
+var contentPattern = new Regex(@"(?<count>\d+) (?<colour>[a-z ]*?) bags?");
+var rules = input.Select(Extract).ToArray();
+
+
+bool Cancontain(string colour, BagRule rule, HashSet<string> visitedColours)
+{
+    if (visitedColours.Contains(rule.Colour))
+    {
+        return false;
+    }
+    if (rule.CanContain.Any(x => x.Colour == colour))
+    {
+        return true;
+    }
+
+    return rule.CanContain.Any(x =>
+        rules.Where(y => y.Colour == x.Colour)
+            .Any(y => Cancontain(colour, y, visitedColours.Concat(new[] {rule.Colour}).ToHashSet())));
+
+}
+
+//foreach (var rule in rules)
+//{
+    //Console.WriteLine($"{Cancontain("shiny gold",rule,new HashSet<string>())} {rule}");
+//}
+
+Console.WriteLine($"Part 1: {rules.Count(x => Cancontain("shiny gold", x, new HashSet<string>()))}");
+// input.txt: 238
+
+
+
+
+
+
+BagRule Extract(string line)
+{
+    var match = containerPattern.Match(line);
+    if (!match.Success)
+    {
+        throw new Exception("Failed to parse " + line);
+    }
+
+    var colour = match.Groups["container_colour"].Value;
+
+
+    return new BagRule
+    {
+        Colour = colour,
+        Input = line,
+        CanContain = contentPattern.Matches(line).Select(x=> (x.Groups["colour"].Value, int.Parse(x.Groups["count"].Value))).ToList()
+    };
+}
+
+record BagRule
+{
+    public string Colour { get; init; }
+    public List<(string Colour, int Count)> CanContain { get; init; } = new();
+    public string Input { get; init; }
+}
+
