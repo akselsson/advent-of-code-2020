e251355df229bf9ee91aac487790c3c790ab2e8f
Patrik Akselsson <patrik.akselsson@tradera.com>
2020-12-08 21:10:00 +0100

    Add dotnet new template

---
+++ Template/Template.cs
@@ -0,0 +1,33 @@
+using System.IO;
+using NUnit.Framework;
+
+namespace Template
+{
+    public class Template
+    {
+        private readonly string Example =
+            @"";
+
+        private readonly string Input = File.ReadAllText("input.txt");
+
+        [Test]
+        public void Example1()
+        {
+        }
+
+        [Test]
+        public void Part1()
+        {
+        }
+
+        [Test]
+        public void Example2()
+        {
+        }
+
+        [Test]
+        public void Part2()
+        {
+        }
+    }
+}
