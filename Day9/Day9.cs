4d0cf0101d57a656163a9893a363cddc33525d9d
Patrik Akselsson <patrik.akselsson@tradera.com>
2020-12-09 08:17:45 +0100

    Day 9 Part 2

--- Day9/Day9.cs
+++ Day9/Day9.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using NUnit.Framework;
@@ -34,21 +35,21 @@ namespace Day9
         [Test]
         public void Example1()
         {
-            var exception = FindException(5, Example);
+            var input = ParseInput(Example);
+            var exception = FindException(5, input);
             Assert.AreEqual(127,exception);
         }
 
-        private long? FindException(int preamble, string input)
+        private long? FindException(int preamble, long[] input)
         {
-            var numbers = input.Split(Environment.NewLine,StringSplitOptions.RemoveEmptyEntries).Select(x => long.Parse(x)).ToArray();
-            for (int i = preamble; i < numbers.Length; i++)
+            for (int i = preamble; i < input.Length; i++)
             {
                 bool match = false;
                 for (int j = i-preamble; j < i; j++)
                 {
                     for (int k = j+1; k < i; k++)
                     {
-                        if (numbers[j] + numbers[k] == numbers[i])
+                        if (input[j] + input[k] == input[i])
                         {
                             match = true;
                         }
@@ -57,7 +58,7 @@ namespace Day9
 
                 if (!match)
                 {
-                    return numbers[i];
+                    return input[i];
                 }
             }
 
@@ -65,20 +66,69 @@ namespace Day9
 
         }
 
+        private static long[] ParseInput(string input)
+        {
+            var numbers = input.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries).Select(x => long.Parse(x))
+                .ToArray();
+            return numbers;
+        }
+
         [Test]
         public void Part1()
         {
-            Assert.AreEqual(1492208709,FindException(25,Input));
+            Assert.AreEqual(1492208709,FindException(25, ParseInput(Input)));
         }
 
         [Test]
         public void Example2()
         {
+            Assert.AreEqual(62,FindWeakness(5, Example));
         }
-
+        
         [Test]
         public void Part2()
         {
+            Assert.AreEqual(238243506,FindWeakness(25, Input));
+        }
+
+        private long? FindWeakness(int preamble, string input)
+        {
+            var numbers = ParseInput(input);
+            var exception = FindException(preamble, numbers);
+            
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                var weakness = FindWeaknessRecursive(new[] {numbers[i]}, numbers.Skip(i + 1).ToArray(), exception.Value);
+                if (weakness.HasValue)
+                {
+                    return weakness;
+                }
+            }
+
+            return null;
+        }
+
+        private long? FindWeaknessRecursive(long[] candidates, IEnumerable<long> input, in long exception)
+        {
+            var sum = candidates.Sum();
+            if (sum == exception)
+            {
+                var sorted = candidates.OrderBy(x => x).ToArray();
+                return sorted.First() + sorted.Last();
+            }
+
+            if (sum > exception)
+            {
+                return null;
+            }
+
+            if (!input.Any())
+            {
+                return null;
+            }
+
+            var next = input.First();
+            return FindWeaknessRecursive(candidates.Concat(new[] {next}).ToArray(), input.Skip(1), exception);
         }
     }
 }
